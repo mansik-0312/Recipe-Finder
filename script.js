e95493ec51ba8e9eece04d0fd8205e2fef749fac
Mansi Kadam <mansi.k@prometteur.in>
2025-03-28 04:51:09 +0530

    Updated CSS and added link to index.html

--- script.js
+++ script.js
@@ -63,26 +63,3 @@ function initialize() {
     }); 
 }
 document.addEventListener("DOMContentLoaded", initialize);
-
-
-
-//     function displayRecipes(recipes) {
-//         resultsContainer.innerHTML = "";
-//         if (recipes.length === 0) {
-//             resultsContainer.innerHTML = "<p>No recipes found</p>";
-//             return;
-//         }
-//         recipes.forEach((recipe) => {
-//             const recipeCard = document.createElement('div');
-//             recipeCard.classList.add('recipe-card');
-//             recipeCard.innerHTML = `
-//             <h3>${recipe.name}</h3>
-//             <p><strong>Difficult:</strong> ${recipe.difficulty}</p>
-//             <p><strong>Time:</strong> ${recipe.prepTimeMinutes + recipe.cookTimeMinutes} min</p>
-//             <p><strong>Ingredients:</strong>${recipe.ingredients.join(",")}</p>
-//             <p><strong>Instructions:</strong> ${recipe.instructions.join("<br>")}</p>
-//             `;
-//             resultsContainer.appendChild(recipeCard);
-//         });
-//     }
-// });
