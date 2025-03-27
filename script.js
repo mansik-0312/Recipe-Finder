6843f7b88aa2bd6faf584f1135dbe295ca6a714b
Mansi Kadam <mansi.k@prometteur.in>
2025-03-28 03:44:16 +0530

    Added function to display recipe and view recipe

--- script.js
+++ script.js
@@ -14,12 +14,63 @@ function fetchRecipes(callback){
         .catch(error => console.error("Error loading recipes:", error));
 }
 
+// Search recipes by ingredient
 function searchRecipes (recipes) {
     const input = document.getElementById('ingredients').value.toLowerCase();
     const results = recipes.filter((recipes) => 
     recipes.ingredients.some(ingredients => ingredients.toLowerCase().includes(input))
     );
     displayRecipes(results);
+}
+
+// Display recipe result
+function displayResult(results) {
+    const container = document.getElementById('results');
+    container.innerHTML = '';
+
+    if (recipes.length === 0) {
+        container.innerHTML = "<p>No recipes found</p>";
+        return;
+    }
+
+    results.forEach((recipes) => {
+        const card = document.getElementById('div');
+        card.classList.add('recipe-card');
+        card.innerHTML = `
+        <div class="recipe-card" onclick="viewRecipeDetails(${recipes.id})">
+        <img src="${recipes.image}" alt="${recipes.name}">
+        <h3>${recipes.name}</h3>
+        <p><strong>Click to view the details</p>
+        </div>
+        `;
+        container.appendChild(card);
+    });
+}
+
+// View detailed recipe instructions
+function viewRecipeDetails(recipeId) {
+    fetch("recipe.json")
+    .then(response => response.json())
+    .then(recipes => {
+        const recipe = recipes.find(r => r.id === recipeId);
+        if (recipe) {
+            const detailContainer = document.getElementById("recipeDetail");
+            detailContainer.innerHTML = `
+            <div class="recipe-detail">
+                <h2>${recipe.name}</h2>
+                <img src="${recipe.image}" alt="${recipe.name}">
+                <p><strong>Ingredient:</strong> ${recipe.ingredients.join(",")}</p>
+                <p><strong>Instruction:</strong>${recipe.instructions}</p>
+                <button onclick="closeRecipeDetails()">Close</button>
+            </div>
+            `;
+            detailContainer.style.display = 'block';
+        }
+    });
+}
+
+function closeRecipeDetails() {
+    document.getElementById('recipeDetail').style.display = 'none';
 }
 
 function initialize() {
