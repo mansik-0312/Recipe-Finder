bc34174a6d13fe1e3db2d618ece8dcdf35989509
Mansi Kadam <mansi.k@prometteur.in>
2025-03-28 03:12:47 +0530

    Added function initialize to initialize application

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
 // Fetch recipes from local JSON file and log data to console
-document.addEventListener("DOMContentLoaded", function () {
-    const searchButton = document.getElementById("searchButton");
-    const ingredientsInput = document.getElementById("ingredients");
-    const resultsContainer = document.getElementById("results");
-
-    fetch("recipes.json")
+// document.addEventListener("DOMContentLoaded", function () {
+//     const searchButton = document.getElementById("searchButton");
+//     const ingredientsInput = document.getElementById("ingredients");
+//     const resultsContainer = document.getElementById("results");
+// function fetchRecipes(callback) {
+function fetchRecipes(callback){
+        fetch("recipes.json")
         .then(response => response.json())
         .then(data => {
             console.log("recipe loded", data);
-            window.recipes = data;
+            callback(data);
         })
         .catch(error => console.error("Error loading recipes:", error));
+}
 
-    function searchRecipes (recipes) {
-        const input = document.getElementById('ingredients').value.toLowerCase();
-        const results = recipes.filter((recipes) => 
-        recipes.ingredients.some(ingredients => ingredients.toLowerCase().includes(input))
-        );
-        displayRecipes(results);
-    }
+function searchRecipes (recipes) {
+    const input = document.getElementById('ingredients').value.toLowerCase();
+    const results = recipes.filter((recipes) => 
+    recipes.ingredients.some(ingredients => ingredients.toLowerCase().includes(input))
+    );
+    displayRecipes(results);
+}
 
-    function displayRecipes(recipes) {
-        resultsContainer.innerHTML = "";
-        if (recipes.length === 0) {
-            resultsContainer.innerHTML = "<p>No recipes found</p>";
-            return;
-        }
-        recipes.forEach((recipe) => {
-            const recipeCard = document.createElement('div');
-            recipeCard.classList.add('recipe-card');
-            recipeCard.innerHTML = `
-            <h3>${recipe.name}</h3>
-            <p><strong>Difficult:</strong> ${recipe.difficulty}</p>
-            <p><strong>Time:</strong> ${recipe.prepTimeMinutes + recipe.cookTimeMinutes} min</p>
-            <p><strong>Ingredients:</strong>${recipe.ingredients.join(",")}</p>
-            <p><strong>Instructions:</strong> ${recipe.instructions.join("<br>")}</p>
-            `;
-            resultsContainer.appendChild(recipeCard);
+function initialize() {
+    fetchRecipes(function (recipes) {
+        document.getElementById('searchButton').addEventListener('click', function() {
+            searchRecipes(recipes);
         });
-    }
-});
+    }); 
+}
+document.addEventListener("DOMContentLoaded", initialize);
+
+
+
+//     function displayRecipes(recipes) {
+//         resultsContainer.innerHTML = "";
+//         if (recipes.length === 0) {
+//             resultsContainer.innerHTML = "<p>No recipes found</p>";
+//             return;
+//         }
+//         recipes.forEach((recipe) => {
+//             const recipeCard = document.createElement('div');
+//             recipeCard.classList.add('recipe-card');
+//             recipeCard.innerHTML = `
+//             <h3>${recipe.name}</h3>
+//             <p><strong>Difficult:</strong> ${recipe.difficulty}</p>
+//             <p><strong>Time:</strong> ${recipe.prepTimeMinutes + recipe.cookTimeMinutes} min</p>
+//             <p><strong>Ingredients:</strong>${recipe.ingredients.join(",")}</p>
+//             <p><strong>Instructions:</strong> ${recipe.instructions.join("<br>")}</p>
+//             `;
+//             resultsContainer.appendChild(recipeCard);
+//         });
+//     }
+// });
