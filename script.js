b3b048c2013336101dc31043c381fc89392860ac
Mansi Kadam <mansi.k@prometteur.in>
2025-03-28 05:17:01 +0530

    Made class RecipeApp and initialize async

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+import RecipeService from "./RecipeService.js";
+import RecipeUI from "./RecipeUI.js";
+
+class RecipeApp {
+    constructor () {
+        this.recipeService = new RecipeService();
+        this.recipeUI = new RecipeUI();
+    }
+
+    async initialize() {
+        await this.recipeService.fetchRecipes();
+
+        document.getElementById('searchButton').addEventListener('click', () => {
+            const ingredient = document.getElementById('ingredients').value;
+            const results = this.recipeService.searchRecipes(ingredient);
+            this.recipeUI.displayResults(results);
+        });
+    }
+}
+
+const app = new RecipeApp();
+app.initialize();
+
 function fetchRecipes(callback){
         fetch("recipes.json")
         .then(response => response.json())
